14c1ed6176855c1d286ee35c130064d56581d429
abhishek4nz <abhishek4nz@gmail.com>
2023-02-14 17:01:53 +0530

    My 2nd Junit Test iteration

--- src/test/java/junitdemo/JunitTest2.java
+++ src/test/java/junitdemo/JunitTest2.java
@@ -5,38 +5,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)         //FOR CREATING A SINGLE INSTANCE FOR ALL TESTS
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class JunitTest2 {
 
      WebDriver driver;
 
     @BeforeAll
-    public void first() {
+     void first() {
         System.setProperty("webdriver.chrome.driver","//home//innofied//Documents//Selenium//Drivers//chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
     @AfterEach
-    public void addPause() throws InterruptedException {
+     void addPause() throws InterruptedException {
         Thread.sleep(1000);
     }
 
     @Test()
+    @Order(1)
     @DisplayName("OPEN THE DESIRED URL")
-    public void second()    {
+     void second()    {
         driver.get("https://saucedemo.com");
     }
 
     @Test
+    @Order(2)
     @DisplayName("LOGIN TO THE APPLICATION")
-    public void third() {
+     void third() {
         driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[1]/input[1]")).sendKeys("standard_user");
         driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//*[@id='login-button' or @name='login-button']")).click();
     }
     @AfterAll
-    public void teardown()  {
+     void teardown()  {
         driver.close();
     }
 }

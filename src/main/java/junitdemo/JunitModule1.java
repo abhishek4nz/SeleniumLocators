174214e5deb42ccf2f3378ed0f15b9571cac2622
abhishek4nz <abhishek4nz@gmail.com>
2023-02-14 18:08:20 +0530

    My 3rd iteration using maven sure fire plugin to run tests using maven.

---
+++ src/main/java/junitdemo/JunitModule1.java
@@ -0,0 +1,13 @@
+package junitdemo;
+
+public class JunitModule1 {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "//home//innofied//Documents//Selenium//Drivers//chromedriver");
+
+//        WebDriver driver;
+
+    }
+
+}
---
+++ src/test/java/junitdemo/Junittest1.java
@@ -0,0 +1,24 @@
+package junitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Junittest1  {
+
+   static {System.setProperty("webdriver.chrome.driver","//home//innofied//Documents//Selenium//Drivers//chromedriver");
+   }
+       WebDriver driver=new ChromeDriver();
+
+    @Test
+    public void firsttest() {
+
+        driver.manage().window().maximize();
+        System.out.println("Lets create first junit test");
+    }
+
+    @Test
+    public void secondtest()    {
+        driver.get("https://saucedemo.com");
+    }
+}

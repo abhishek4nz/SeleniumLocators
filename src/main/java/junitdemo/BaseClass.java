cb7a8ac759ff88876f8281c52873d2bd10950a81
abhishek4nz <abhishek4nz@gmail.com>
2023-02-16 16:03:10 +0530

    My 3rd Junit Test iteration

---
+++ src/main/java/junitdemo/BaseClass.java
@@ -0,0 +1,9 @@
+package junitdemo;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass { static WebDriver driver; }
+
+//CREATED THIS STATIC DRIVER OBJECT
+//EXTENDED THIS OBJECT IN JUNIT TESTS
+
--- src/test/java/junitdemo/JunitTest2.java
+++ src/test/java/junitdemo/JunitTest2.java
@@ -2,44 +2,53 @@ package junitdemo;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
-@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)         //FOR CREATING A SINGLE INSTANCE FOR ALL TESTS
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class JunitTest2 {
+    @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)        //FOR CREATING A SINGLE INSTANCE FOR ALL TESTS
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    @DisplayName("JUNIT TEST SUITE")
 
-     WebDriver driver;
+    public class JunitTest2 extends BaseClass {
 
     @BeforeAll
-     void first() {
+     void setup() {
         System.setProperty("webdriver.chrome.driver","//home//innofied//Documents//Selenium//Drivers//chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @AfterEach
-     void addPause() throws InterruptedException {
-        Thread.sleep(1000);
-    }
+     void addPause() throws InterruptedException { Thread.sleep(2000); }
 
-    @Test()
+    @RepeatedTest(1)                     //REPEATS THE TEST ONCE MORE
+    @DisplayName("OPEN THE DESIRED URL")            //NAME OF TEST
     @Order(1)
-    @DisplayName("OPEN THE DESIRED URL")
-     void second()    {
+     void first()    {
         driver.get("https://saucedemo.com");
     }
 
     @Test
-    @Order(2)
     @DisplayName("LOGIN TO THE APPLICATION")
-     void third() {
+    @Disabled("LETS GET IT DISABLED")
+    @Order(2)
+     void second() {
         driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[1]/input[1]")).sendKeys("standard_user");
         driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//*[@id='login-button' or @name='login-button']")).click();
     }
+
+    @Test
+    @Tag("group")       //USAGE OF GROUP
+    void third()   {
+//        System.out.println("Will this run only?");
+        driver.get("https://google.com");
+    }
+
     @AfterAll
      void teardown()  {
         driver.close();
     }
 }
+
